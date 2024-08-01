23d49a02da3ea82ede48e29178f9308366bf77b3
MissTea2 <me@misstea2.live>
2024-08-01 21:33:15 +0800

    update

---
+++ chunk-K4RKSFWC.js
@@ -0,0 +1,1 @@
+import"./chunk-ODN5LVDJ.js";var t=[{path:"",redirectTo:"demo",pathMatch:"full"},{path:"",title:"\u4E86\u89E3 Angela",children:[{path:"demo",loadChildren:()=>import("./chunk-4YBNX5BD.js")},{path:"design",loadChildren:()=>import("./chunk-LSSMN2G6.js")},{path:"overview",loadChildren:()=>import("./chunk-RQY67GT2.js")}]}],e=t;export{e as default};
